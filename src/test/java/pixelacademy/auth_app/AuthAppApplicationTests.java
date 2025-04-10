167b647d33304371c72a1698d31a36945ebfc24a
Пользователь <abror.nabiyev321@gmail.com>
2025-04-10 18:52:26 +0300

    Initial commit.

---
+++ src/test/java/pixelacademy/auth_app/AuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pixelacademy.auth_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
